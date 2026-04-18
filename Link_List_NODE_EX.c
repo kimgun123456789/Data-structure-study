e6fccbdafab1f71102fb57b886d68a1c03c6293f
kimgun123456789 <127281472+kimgun123456789@users.noreply.github.com>
2026-04-18 17:19:25 +0900

    Create Link_List_NODE_EX.c

---
+++ Link_List_NODE_EX.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+//문자열 저장 연결 리스트
+#define MAX 100
+
+typedef struct element {
+	char name[100];
+}element;
+
+typedef struct ListNode {
+	element data;
+	struct ListNode *link;
+}ListNode;
+
+void error(char* message) {
+	fprintf(stderr, "%s\n", message);
+	exit(1);
+}
+
+ListNode* insert_first(ListNode* head, element value) {
+	ListNode* p = (ListNode*)malloc(sizeof(ListNode));
+	p->data = value;
+	p->link = head;
+	head = p;
+	return head;
+}
+
+void print_list(ListNode* head) {
+	for (ListNode* p = head; p!= NULL; p = p->link) {
+		printf("%s->", p->data.name);
+	}
+	printf("NULL\n");
+}
+
+int main(void) {
+
+	ListNode* head = NULL;
+	element data;
+
+	strcpy(data.name, "APPLE");
+	head = insert_first(head, data);
+	print_list(head);
+
+	strcpy(data.name, "BANANA");
+	head = insert_first(head, data);
+	print_list(head);
+	return 0;
+}
